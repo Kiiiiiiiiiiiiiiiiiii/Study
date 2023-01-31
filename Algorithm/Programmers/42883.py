ca40173874e79a14c3edb7eb6b046693c83c2ca2
Kii_ <37584855+Kiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2023-01-31 17:25:46 +0900

    Create 42883.py

---
+++ Algorithm/Programmers/42883.py
@@ -0,0 +1,13 @@
+def solution(number, k):
+    list = []
+    
+    for i in number:
+        while list and list[-1] < i and k > 0:
+            k -= 1
+            list.pop()
+        list.append(i)
+        
+    if k != 0:
+        list = list[:-k]
+
+    return ''.join(list)
