a284779717dee22346f609c37bad08a75078ffb4
Kiiiiiiiiiiiiiii <choys0199@gmail.com>
2023-05-02 00:35:56 +0900

    [구현] 알파벳 개수

---
+++ Algorithm/BOJ/10808.py
@@ -0,0 +1,8 @@
+s = input()
+
+atoz = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+result = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+for i in range(len(s)):
+    result[atoz.index(s[i])]+=1
+
+print(*result)
