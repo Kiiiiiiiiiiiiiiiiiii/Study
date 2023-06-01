57d766426d2c401ee5f9e6b764066565db44a059
Kiiiiiiiiiiiiiii <choys0199@gmail.com>
2023-06-01 23:05:27 +0900

    [백트래킹] N과 M (2)

---
+++ Algorithm/BOJ/15650.py
@@ -0,0 +1,14 @@
+n, m = list(map(int, input().split()))
+a = []
+# 4 2
+def dfs(start):
+    if len(a) == m:
+        print(' '.join(map(str, a)))
+        return
+    for i in range(start, n + 1):
+        if i not in a:
+            a.append(i)
+            dfs(i+1)
+            a.pop()
+
+dfs(1)
