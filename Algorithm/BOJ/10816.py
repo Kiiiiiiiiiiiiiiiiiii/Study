e6a74d492e0f8c4f20902c458fafc2fa0189b1e0
Kiiiiiiiiiiiiiii <choys0199@gmail.com>
2023-06-16 03:30:27 +0900

    [자료구조] 숫자 카드 2 복습

--- Algorithm/BOJ/10816.py
+++ Algorithm/BOJ/10816.py
@@ -1,18 +1,20 @@
 n = int(input())
-nlist = list(map(int, input().split()))
+a = list(map(int, input().split()))
 m = int(input())
-mlist = list(map(int, input().split()))
+b = list(map(int, input().split()))
 
-ans = {}
+arry={}
 
-for i in nlist:
-    if i in ans:
-        ans[i] += 1
+#a를 k v로 arry에 저장
+for i in a:
+    if i in arry:
+        arry[i] += 1
     else:
-        ans[i] = 1
+        arry[i] = 1
 
-for i in mlist:
-    if i in ans:
-        print(ans[i], end = ' ')
+# arry에 b의 값이 있으면 출력
+for i in b:
+    if i in arry:
+        print(arry[i], end=' ')
     else:
-        print(0, end = ' ')
+        print(0, end=' ')
