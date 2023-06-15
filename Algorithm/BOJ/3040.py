58eb57dd77db7dbd8435a070769202028c4a1083
Kiiiiiiiiiiiiiii <choys0199@gmail.com>
2023-06-15 16:51:40 +0900

    [구현] 백설 공주와 일곱 난쟁이 복습

--- Algorithm/BOJ/3040.py
+++ Algorithm/BOJ/3040.py
@@ -1,13 +1,18 @@
 from itertools import combinations
+
 array = []
 tmp =[]
+
 for _ in range(9):
     array.append(int(input()))
 
-# 9개 중에 7개를 뽑는 조합
 tmp = list(combinations(array, 7))
 
 for i in tmp:
     if sum(i) == 100:
         for j in i:
             print(j)
+
+
+
+        
