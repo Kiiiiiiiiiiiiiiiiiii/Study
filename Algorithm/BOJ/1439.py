45c80e4d17eb5f92f2ef558aca4e7838d6328f2f
Kiiiiiiiiiiiiiii <choys0199@gmail.com>
2023-06-26 18:43:15 +0900

    [그리디] 뒤집기

--- Algorithm/BOJ/1439.py
+++ Algorithm/BOJ/1439.py
@@ -1,19 +1,17 @@
 n = str(input())
 
-o = n.split('1')
-z = n.split('0')
+one = n.split('1')
+zero = n.split('0')
 
-cnt_0 = 0
-cnt_1 = 0
+cnt1 = 0
+cnt0 = 0
 
-
-for i in o:
+for i in one:
     if '0' in i:
-        cnt_1 += 1
-    
+        cnt1 += 1
 
-for j in z:
-    if '1' in j:
-        cnt_0 += 1
+for i in zero:
+    if '1' in i:
+        cnt0 += 1
 
-print(min(cnt_0, cnt_1))
+print(min(cnt0, cnt1))
