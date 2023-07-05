3eb232b24527c737df020fc3f7b8ac258d9bb611
Kiiiiiiiiiiiiiii <choys0199@gmail.com>
2023-07-05 23:17:57 +0900

    [DFS/BFS] DFS와 BFS

--- Algorithm/BOJ/25644.py
+++ Algorithm/BOJ/25644.py
@@ -3,12 +3,7 @@ import sys
 n = int(sys.stdin.readline())
 list = list(map(int, sys.stdin.readline().split()))
 
-min, ans = list[0], 0
-
 for i in range(1, n):
-  if min < list[i]:
-    ans = max(ans,list[i]-min)
-  else:
-    min = list[i]
-
-print(ans)
+    list[i] = max(list[i], list[i] + list[i-1])
+    
+print(max(list))
